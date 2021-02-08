5b9bc37d4acd17f426ef3e2ce7ed844cc46be4dc
Praveenchowdaryy <55449438+Praveenchowdaryy@users.noreply.github.com>
2021-02-08 09:57:35 +0530

    Add files via upload

---
+++ DateBase/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿USE master;
+
+GO
+-- Add a message in English and French 
+EXEC sp_addmessage @msgnum = 60000, @severity = 16,
+	@msgtext=N'The item named %s already exists in %.',
+	@lang= 'us_english';
+EXEC sp_addmessage @msgnum = 60000, @severity = 16, 
+	@msgtext = N'L''élément nommé %1! existe déjà dans %2!',
+	@lang=French ;
+GO
